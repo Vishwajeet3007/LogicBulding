1a3d9377ee440afeb95b3d51da3098c594610abc
Vishwajeet Kumar <vishwa843113@gmail.com>
2025-03-11 09:16:52 +0530

    Create SecondLarget.py

---
+++ SecondLarget.py
@@ -0,0 +1,15 @@
+def secondLarget(arr):
+    if len(arr) < 2:
+        return "Array must have at least two distinct elements."
+    first = float('-inf')  # Smallest possible value
+    second = float('-inf') 
+    for i in range(len(arr)):
+        if arr[i] > first:
+            second = first
+            first = arr[i]
+        elif arr[i] > second:
+            second = arr[i]
+    return second
+arr = [4,56,7,4,26,77]
+result = secondLarget(arr)
+print("Second Larget : ",result)
